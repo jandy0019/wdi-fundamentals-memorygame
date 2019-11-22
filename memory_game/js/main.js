90eb9151efddacfdb803fc26a809d7a8d12c101c
Juice Cx <juicecx@Juices-MacBook-Pro.local>
2019-11-21 20:18:59 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,26 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+const cards = ["queen", "queen", "king", "king"]
+const cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[3]
+cardsInPlay.push('cardOne')
+cardsInPlay.push('cardTwo')
+console.log("User flipped queen")
+console.log("User flipped king")
+
+if (cardsInPlay === 2)
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match.");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+
+
+
+
