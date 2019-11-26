3b32a4b41829537733b713b6660d146cb4af8960
Juice Cx <juicecx@Juices-MacBook-Pro-2.local>
2019-11-25 22:58:11 -0800

    attempted to create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,7 +23,7 @@ const cards = [
 }
 ];
 
-const cardsInPlay = [];
+var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -33,12 +33,35 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardID) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-	console.log("User flipped" + cards[cardId].rank);
+	
+	console.log("User flipped" + " " + cards[cardId].rank);
 	console.log(cards.cardImage);
 	console.log(cards.suit);
+
+	cardId = this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-data', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+
+
+
+
+
